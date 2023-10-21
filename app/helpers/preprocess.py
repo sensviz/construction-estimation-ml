569a0b3f10e7e9b03f0b1e1d57f0ebaf9a892959
Maria Zafar <69984290+maria-zafar@users.noreply.github.com>
2023-10-22 03:20:02 +0500

    new cost estimation module

--- app/helpers/preprocess.py
+++ app/helpers/preprocess.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 import numpy as np
+import torch
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 def preprocess():
     df = pd.read_csv("cost_estimation.csv")
@@ -22,6 +25,30 @@ def preprocess():
        'area'])
     
     return df2
+
+def label_encode_categorical_values(X):
+    label = LabelEncoder()
+    X['id'] = label.fit_transform(X.id.values)
+    return X
+
+def one_hot_encoding(X):
+    one_hot_encoded = pd.get_dummies(X)
+    X=one_hot_encoded
+    return X
+
+# Load the data
+data = pd.read_excel('cost_estimation.xlsx')
+
+data=label_encode_categorical_values(data)
+data=one_hot_encoding(data)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data.drop('cost', axis=1), data['cost'], test_size=0.2, random_state=42)
+# Convert the data to PyTorch tensors
+X_train = torch.FloatTensor(X_train.values)
+y_train = torch.FloatTensor(y_train.values).view(-1, 1)
+X_test = torch.FloatTensor(X_test.values)
+y_test = torch.FloatTensor(y_test.values).view(-1, 1)
 
 
 
