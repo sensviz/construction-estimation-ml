fcb71d927ee2e28faa89c2d043ebfd87b3d92215
RAOMMA <raomubashirali@sensviz.com>
2023-10-26 15:01:37 +0500

    code cleaning

--- app/helpers/preprocess.py
+++ app/helpers/preprocess.py
@@ -4,23 +4,23 @@ from sklearn.preprocessing import LabelEncoder , OneHotEncoder
 encoder = OneHotEncoder()
 columns_to_encode = ['product', 'material']
 
-def label_encode_categorical_values(X):
-    label = LabelEncoder()
-    X['id'] = label.fit_transform(X.id.values)
-    return X
+# def label_encode_categorical_values(X):
+#     label = LabelEncoder()
+#     X['id'] = label.fit_transform(X.id.values)
+#     return X
 
-def one_hot_encoding(X):
-    one_hot_encoded = pd.get_dummies(X)
-    X=one_hot_encoded
-    return X
+# def one_hot_encoding(X):
+#     one_hot_encoded = pd.get_dummies(X)
+#     X=one_hot_encoded
+#     return X
 
-def preprocess(): 
+# def preprocess(): 
 
-    # Load the data
-    data = pd.read_excel('cost_estimation.xlsx')
-    data=label_encode_categorical_values(data)
-    data=one_hot_encoding(data)
-    data.to_csv('preprocessed_data.csv', index=False) 
+#     # Load the data
+#     data = pd.read_excel('cost_estimation.xlsx')
+#     data=label_encode_categorical_values(data)
+#     data=one_hot_encoding(data)
+#     data.to_csv('preprocessed_data.csv', index=False) 
 
 
 
